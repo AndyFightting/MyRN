c2ab7a554695c3842c9b49d70a32a172b7573d79
guimingsu <2048396401@qq.com>
2016-12-20 16:50:55 +0800

    image
    image

--- ios/View/TxlView.js
+++ ios/View/TxlView.js
@@ -8,28 +8,88 @@ import {
     View,
     Text,
     Alert,
+    Image,
     ScrollView,
+    TouchableWithoutFeedback,
+    ListView,
+    Dimensions,
+    PixelRatio,
 } from 'react-native';
-import TitleView from './TabTitleView';
 import MenuView from './MenuView';
 import HeaderView from './HeaderView';
+import ViewPager from 'react-native-viewpager';
 
+const BANNER_IMGS = [
+    require('../../images/1.jpg'),
+    require('../../images/2.jpg'),
+    require('../../images/3.jpg'),
+    require('../../images/4.jpg'),
+];
+const len = 160;
 
 export default class TxlView extends Component {
 
     // 构造
     constructor(props) {
         super(props);
-        // 初始状态
-        this.state = {};
+
+
+        var listDataBlob = [0,1,2,3];
+
+        var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+        var dataSource = new ViewPager.DataSource({
+            pageHasChanged: (p1, p2) => p1 !== p2,
+        });
+        this.state = {
+            screenWidth: this.screenWidth(),
+            screenHeight: this.screenHeight(),
+            pxRadio: this.pxRatio(),
+            dataSource: dataSource.cloneWithPages(BANNER_IMGS),
+            listData: ds.cloneWithRows(listDataBlob),
+        };
     }
+
+
+    screenWidth() {
+        return Dimensions.get('window').width;
+    }
+
+    screenHeight() {
+        return Dimensions.get('window').height;
+    }
+
+    pxRatio() {
+        return PixelRatio.get();//一个点有几个像素，像android的一个px和dp比例似的
+    }
+
+
 
     render() {
         return (
             <View style={styles.container}>
                 <HeaderView/>
 
-                <ScrollView automaticallyAdjustContentInsets={false}>
+                <ListView
+                    automaticallyAdjustContentInsets={false}
+                    style={styles.listView}
+                    renderHeader={this.renderHeader}
+                    renderRow={this.listItem.bind(this)}
+                    contentContainerStyle={styles.contentStyle}
+                    dataSource={this.state.listData}/>
+            </View>
+        );
+    }
+
+        renderHeader = () => {
+        return(
+            <View>
+                <ViewPager
+                    style={{height:130}}
+                    dataSource={this.state.dataSource}
+                    renderPage={this.renderPage}
+                    isLoop={true}
+                    autoPlay={true}/>
                 <View style={styles.menuBackView}>
                     <MenuView name='充话费' image={require('../../images/icon4.png')} onPress={()=>this.menuPress(0)}/>
                     <MenuView name='看电影' image={require('../../images/icon5.png')} onPress={()=>this.menuPress(1)}/>
@@ -44,8 +104,40 @@ export default class TxlView extends Component {
                     <MenuView name='电视剧' image={require('../../images/icon5.png')} onPress={()=>this.menuPress(8)}/>
                     <MenuView name='充值' image={require('../../images/icon4.png')} onPress={()=>this.menuPress(9)}/>
                 </View>
-                </ScrollView>
             </View>
+        );
+    }
+
+    listItem(rowData: string,
+             sectionID: number,
+             rowID: number,
+             highlightRow: (sectionID: number, rowID: number) => void) {
+        let itemWidth = this.state.screenWidth / 3;
+        return (
+            <View style={{height: 100,flexDirection:'row',marginBottom:10}}>
+                {this.listItemView()}
+                {this.listItemView()}
+                {this.listItemView()}
+            </View>
+        );
+    };
+
+    listItemView(){
+        return(
+            <View style={{flex:1}}>
+                <Image style={{height:80,width:this.state.screenWidth/-10,margin:5}} source={require('../../images/3.jpg')}/>
+                <Text style={{alignSelf:'center'}}>iPhone</Text>
+
+            </View>
+        );
+    };
+
+
+    renderPage(data, pageID) {
+        return (
+            <Image
+                source={data}
+                style={styles.page}/>
         );
     }
 
@@ -56,6 +148,10 @@ export default class TxlView extends Component {
 };
 
 const styles = StyleSheet.create({
+    page: {
+        flex: 1,
+        height: 130,
+    },
     container: {
         flex: 1,
         backgroundColor: '#efeef4',
@@ -66,7 +162,11 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         paddingTop: 8,
         paddingBottom: 8,
-    }
+    },
+    listView:{
+      marginBottom:50,
+        paddingBottom:10,
+    },
 
 
 });
